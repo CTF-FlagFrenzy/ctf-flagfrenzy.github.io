3541de1181ececa520b8b6ab744837910d21fad3
zVSciy <88750044+zVSciy@users.noreply.github.com>
2025-03-30 23:42:24 +0200

    Add new challenges: Hidden Job, Pixel Spy, Pycked Apart, Security Maths, Profile Prodigy, Corrupted File, Shell Assembly, Ceasar Cypher, Stealth Invoice, Mental Overflow, Behind The Pixels, Capture The Bytes, Phantom Frequency, Binary Puzzeling, Time Travelers Hunt

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,72 +67,72 @@ export default defineConfig({
         {
 					label: 'Challenges',
 					items: [
-            { label: 'Challenges | Overview', link: '/challenges/overview' },
+            { label: 'Challenges | Overview', link: '/challenge/overview' },
             { 
               label: 'Web Challenge',
               items: [
-                { label: 'Solana Assets', link: '/challenges/web-challenge/solana-assets' },
-                { label: 'The SQL Heist', link: '/challenges/web-challenge/the-sql-heist' },
-                { label: 'File And Seek', link: '/challenges/web-challenge/file-and-seek' },
-                { label: 'Shadow File', link: '/challenges/web-challenge/shadow-file' },
+                { label: 'Solana Assets', link: '/challenge/web-challenge/solana-assets' },
+                { label: 'The SQL Heist', link: '/challenge/web-challenge/the-sql-heist' },
+                { label: 'File And Seek', link: '/challenge/web-challenge/file-and-seek' },
+                { label: 'Shadow File', link: '/challenge/web-challenge/shadow-file' },
               ]
             },
             {
               label: 'Others',
               items: [
-                { label: 'Security Maths', link: '/challenges/others/security-maths' },
-                { label: 'Hidden Job', link: '/challenges/others/hidden-job' },
-                { label: 'Decryption Master', link: '/challenges/others/decryption-master' },
-                { label: 'The Neverending Zip', link: '/challenges/others/the-neverending-zip' },
+                { label: 'Security Maths', link: '/challenge/others/security-maths' },
+                { label: 'Hidden Job', link: '/challenge/others/hidden-job' },
+                { label: 'Decryption Master', link: '/challenge/others/decryption-master' },
+                { label: 'The Neverending Zip', link: '/challenge/others/the-neverending-zip' },
               ]
             },
             {
               label: 'Cryptography',
               items: [
-                { label: 'Ceasar Cypher', link: '/challenges/cryptography/ceasar-cypher' },
-                { label: 'Binary Puzzeling', link: '/challenges/cryptography/binary-puzzeling' },
-                { label: 'Checking Encryption', link: '/challenges/cryptography/checking-encryption' },
+                { label: 'Ceasar Cypher', link: '/challenge/cryptography/ceasar-cypher' },
+                { label: 'Binary Puzzeling', link: '/challenge/cryptography/binary-puzzeling' },
+                { label: 'Checking Encryption', link: '/challenge/cryptography/checking-encryption' },
               ]
             },
             {
               label: 'Reversing',
               items: [
-                { label: 'Confused Scripting', link: '/challenges/reversing/confused-scripting' },
-                { label: 'Mental Overflow', link: '/challenges/reversing/mental-overflow' },
-                { label: 'Shell Assembly', link: '/challenges/reversing/shell-assembly' },
-                { label: 'Corrupted File', link: '/challenges/reversing/corrupted-file' },
-                { label: 'PYCked Apart', link: '/challenges/reversing/pycked-apart' },
+                { label: 'Confused Scripting', link: '/challenge/reversing/confused-scripting' },
+                { label: 'Mental Overflow', link: '/challenge/reversing/mental-overflow' },
+                { label: 'Shell Assembly', link: '/challenge/reversing/shell-assembly' },
+                { label: 'Corrupted File', link: '/challenge/reversing/corrupted-file' },
+                { label: 'PYCked Apart', link: '/challenge/reversing/pycked-apart' },
               ]
             },
             {
               label: 'Forensics',
               items: [
-                { label: 'Pixel Spy', link: '/challenges/forensics/pixel-spy' },
-                { label: 'Behind The Pixels', link: '/challenges/forensics/behind-the-pixels' },
-                { label: 'Stealth Invoice', link: '/challenges/forensics/stealth-invoice' },
-                { label: 'Transmission Received', link: '/challenges/forensics/transmission-received' },
-                { label: 'Phantom Frequency', link: '/challenges/forensics/phantom-frequency' },
-                { label: 'Capture the Bytes', link: '/challenges/forensics/capture-the-bytes' },
+                { label: 'Pixel Spy', link: '/challenge/forensics/pixel-spy' },
+                { label: 'Behind The Pixels', link: '/challenge/forensics/behind-the-pixels' },
+                { label: 'Stealth Invoice', link: '/challenge/forensics/stealth-invoice' },
+                { label: 'Transmission Received', link: '/challenge/forensics/transmission-received' },
+                { label: 'Phantom Frequency', link: '/challenge/forensics/phantom-frequency' },
+                { label: 'Capture the Bytes', link: '/challenge/forensics/capture-the-bytes' },
               ]
             },
             {
               label: 'Steganography',
               items: [
-                { label: 'Hidden Frequencies', link: '/challenges/steganography/hidden-frequencies' },
+                { label: 'Hidden Frequencies', link: '/challenge/steganography/hidden-frequencies' },
               ]
             },
             {
               label: 'OSINT',
               items: [
-                { label: 'Profile Prodigy', link: '/challenges/osint/profile-prodigy' },
-                { label: 'Time Travelers Hunt', link: '/challenges/osint/time-travelers-hunt' },
-                { label: 'Git Secrets', link: '/challenges/osint/git-secrets' },
+                { label: 'Profile Prodigy', link: '/challenge/osint/profile-prodigy' },
+                { label: 'Time Travelers Hunt', link: '/challenge/osint/time-travelers-hunt' },
+                { label: 'Git Secrets', link: '/challenge/osint/git-secrets' },
               ]
             },
             {
               label: 'Blockchain',
               items: [
-                { label: 'Solana Dumb Contract', link: '/challenges/blockchain/solana-dumb-contract' },
+                { label: 'Solana Dumb Contract', link: '/challenge/blockchain/solana-dumb-contract' },
               ]
             }
           ],
